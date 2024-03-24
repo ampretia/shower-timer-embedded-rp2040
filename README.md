4835b3aa6985db0c0f7bf4222335bdc9c274687e
mbw <matthew@mh-white.com>
2024-03-24 09:22:25 +0000

    First update
    Signed-off-by: mbw <matthew@mh-white.com>

--- README.md
+++ README.md
@@ -1,4 +1,79 @@
-# XIAO RP2040 WS2812
-Rust program that blinks the USER LED and RGB LED on a Seeed Studio XIAO RP2040 board using ws2812-pio
-![alt text](https://files.seeedstudio.com/wiki/XIAO-RP2040/img/xinfront.jpg)
+# Showeer Timer - aka Embedded RP2040 with Rust
+
+Self-powered shower timer - inspired by teenagers spending ages in the shower; along with an excuse to build something. 
+
+Saw something similar in a random catalog years ago.
+
+Features:
+
+- Uses water flow generator to create the power needed
+- Compact-ish design to screw into the shower feed
+- RP2040 as the micro-controller
+
+![](./_docs/IMG_20240318_081910.jpg)
+
+## Generator
+
+Search for Water Turbine, and lots of different things turn up on Amazon and eBay.
+
+Decided to take a chance on [this one](https://www.amazon.co.uk/dp/B07WXK59MK?psc=1&ref=ppx_yo2ov_dt_b_product_details)
+
+- Voltage was 12v - had no idea what type of current/voltages would actually come out. Knowing wthat something in 5-3.3v would be needed seemed that opting for 12v would give some room to drop this and get some current.
+- The connectors were 1/2" threaded which is the UK standard for showers.
+
+### Practical Experiments
+
+On testing it seemed that with only a modest water flow it could get to 12v; taking it apart later determined that there was a regulator and capacitor inside that help to regulate the voltage.   Current to power a couple of LEds was quite sufficient.
+
+This was witih a decent mains-level water pressure.
+
+
+
+## Hardware
+
+There's a 5mm tri=colour LED, driven with 3* 2n2222 transistors from the GPIO pins.
+
+For power I had one these very small boards available [SparkFun BabyBuck Regulator Breakout - 5V](https://thepihut.com/products/sparkfun-babybuck-regulator-breakout-5v-ap63357?variant=42391014506691)
+
+The controller I had available was a [Seed XIAO RP2040](https://thepihut.com/products/seeed-xiao-rp2040?variant=41181796335811)
+
+To address the 'elephant-in-the-room' - a dual core processor is way too much; an embedded arduino, or a PIC controller would have been far better. Even smaller and even less power. 
+
+But for 2 reasons; one the only controller that was small enough was the XIAO; plus after I'd managed to unsolder some of the pads previously it wasn't really up for anything needing SPI or I2C. 
+
+Second reason is you can use Rust!
+
+### Additional parts
+
+Note that the brass converter which is internally threaded to 1/2".  so this was needed to connect to the shower outlet - with washer as well. 
+
+The white outer casing a small plastic container from aquarium fish food; cleaned, label removed and cutdown. Glued with circuit inside. 
+
+![](./_docs/IMG_20240318_081917.jpg)
+
+## Software
+
+Contained in this repo based on this [tutorial](https://tutoduino.fr/en/tutorials/programing-in-rust-the-xiao-rp2040-board/)
+
+When the power is connected; the software works through using 3 GPIO pins to control the LED. It'll move through Green-Blue-Red-Flashing Red. Moving ever 4 minutes, and ending on the flashing red.
+
+To build the software please see the tutorial mentioned above, the two main commands I used where
+
+```
+cargo build --release
+elf2uf2-rs target/thumbv6m-none-eabi/debug/xiao_rp2040_rs op2.uf2
+```
+
+Rather than `cargo run` which would to try and download the code to the XIAO, I was running this on WSL2 so the transfer was a manual step.
+
+## Does it work?
+
+![](./_docs/IMG_20240318_082328.jpg)
+![](./_docs/IMG_20240318_082603.jpg)
+
+Bascially Yes; does it reduce the shower time - well maybe it won't long term when the novelty wears off. 
+
+The big issue is if you turn on the water slowly; the XIAO doesn't start properly. This will be connected to the power; and could be fixed by a hardware reset at some point. 
+
+And really it would be so much better to use a different microprocessor (PIC probably) and also use a proper PCB. 
 
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,16 @@
-//! XIAO RP2040 Blinking LED Example
-//!
-//! https://tutoduino.fr/
-//!
-//! Blinks the USER LED and NeoPixel RGB LED on a Seeed Studio XIAO RP2040 board.
-//!
+//! XIAO RP2040 Shower timer
 
 #![no_std]
 #![no_main]
-
-use embedded_hal::digital::v2::OutputPin;
-use hal::pio::PIOExt;
-use hal::Timer;
+use embedded_hal::digital::v2::OutputPin;// use hal::pio::PIOExt;
+// use hal::Timer;
 use panic_halt as _;
 use seeeduino_xiao_rp2040::entry;
 use seeeduino_xiao_rp2040::hal;
 use seeeduino_xiao_rp2040::hal::pac;
 use seeeduino_xiao_rp2040::hal::prelude::*;
-use smart_leds::SmartLedsWrite;
-use smart_leds::RGB8;
-use ws2812_pio::Ws2812;
 
-const LUMINOSITY: u8 = 10;
-const RED: RGB8 = RGB8::new(LUMINOSITY, 0, 0);
-const GREEN: RGB8 = RGB8::new(0, LUMINOSITY, 0);
-const BLUE: RGB8 = RGB8::new(0, 0, LUMINOSITY);
-const WHITE: RGB8 = RGB8::new(LUMINOSITY / 3, LUMINOSITY / 3, LUMINOSITY / 3);
+const PHASE_TIME: u32 = 4 * 60 * 1000;
 
 /// Entry point to our bare-metal application.
 ///
@@ -71,60 +57,88 @@ fn main() -> ! {
     // The delay object lets us wait for specified amounts of time (in
     // milliseconds)
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-    let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
+    // let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
 
     // Setup PIO
-    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+    // let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
 
-    // Setup Neopixel RGB LED
-    let mut ws = Ws2812::new(
-        pins.neopixel_data.into_mode(),
-        &mut pio,
-        sm0,
-        clocks.peripheral_clock.freq(),
-        timer.count_down(),
-    );
+    // inbuilt led
+    let mut built_b_led = pins.led_blue.into_push_pull_output();
+    built_b_led.set_high().unwrap();
 
-    // Power on Neopixel RGB LED
-    let mut neopixel_power = pins.neopixel_power.into_push_pull_output();
-    neopixel_power.set_high().unwrap();
+    let mut built_g_led = pins.led_green.into_push_pull_output();
+    built_g_led.set_high().unwrap();
+
+    let mut built_r_led = pins.led_red.into_push_pull_output();
+    built_r_led.set_high().unwrap();
+
+
+    // --------------------------------------
 
     // Configure the USER LED pins to operate as a push-pull output
-    let mut led_blue_pin = pins.led_blue.into_push_pull_output();
-    let mut led_green_pin = pins.led_green.into_push_pull_output();
-    let mut led_red_pin = pins.led_red.into_push_pull_output();
+    let mut led_blue_pin = pins.scl.into_push_pull_output();
+    let mut led_red_pin = pins.sda.into_push_pull_output();
+    let mut led_green_pin = pins.tx.into_push_pull_output();
+
+    // Set USER LED to blue
+    led_blue_pin.set_low().unwrap();
+    led_red_pin.set_high().unwrap();
+    led_green_pin.set_low().unwrap();
+
+    delay.delay_ms(1000);
+
+    // Set USER LED to red
+    led_blue_pin.set_high().unwrap();
+    led_red_pin.set_low().unwrap();
+    led_green_pin.set_low().unwrap();
+    delay.delay_ms(1000);
+
+    // Set USER LED to blue
+    led_blue_pin.set_low().unwrap();
+    led_red_pin.set_high().unwrap();
+    led_green_pin.set_low().unwrap();
+
+    delay.delay_ms(1000);
+
+    // Set USER LED to green
+    led_blue_pin.set_low().unwrap();
+    led_red_pin.set_low().unwrap();
+    led_green_pin.set_high().unwrap();
+    // Set RGB LED to green
+    delay.delay_ms(1000);
+
+    led_blue_pin.set_high().unwrap();
+    led_red_pin.set_low().unwrap();
+    led_green_pin.set_low().unwrap();
+    delay.delay_ms(1000);
+
+    // ----------------------------------------
+    led_blue_pin.set_low().unwrap();
+    led_red_pin.set_low().unwrap();
+    led_green_pin.set_high().unwrap();
+    delay.delay_ms(PHASE_TIME);
+
+    led_blue_pin.set_high().unwrap();
+    led_red_pin.set_low().unwrap();
+    led_green_pin.set_low().unwrap();
+    delay.delay_ms(PHASE_TIME);
+
+    led_blue_pin.set_low().unwrap();
+    led_red_pin.set_high().unwrap();
+    led_green_pin.set_low().unwrap();
+    delay.delay_ms(PHASE_TIME);
+
+    led_blue_pin.set_low().unwrap();
+    led_green_pin.set_low().unwrap();
 
     loop {
-        // Set USER LED to blue
-        led_blue_pin.set_low().unwrap();
-        led_red_pin.set_high().unwrap();
-        led_green_pin.set_high().unwrap();
-        // Set RGB LED to blue
-        ws.write([BLUE].iter().copied()).unwrap();
-        delay.delay_ms(500);
-
-        // Set USER LED to red
-        led_blue_pin.set_high().unwrap();
-        led_red_pin.set_low().unwrap();
-        led_green_pin.set_high().unwrap();
-        // Set RGB LED to red
-        ws.write([RED].iter().copied()).unwrap();
-        delay.delay_ms(500);
-
-        // Set USER LED to green
-        led_blue_pin.set_high().unwrap();
-        led_red_pin.set_high().unwrap();
-        led_green_pin.set_low().unwrap();
-        // Set RGB LED to green
-        ws.write([GREEN].iter().copied()).unwrap();
-        delay.delay_ms(500);
-
         // Set USER LED to white
-        led_blue_pin.set_low().unwrap();
-        led_red_pin.set_low().unwrap();
-        led_green_pin.set_low().unwrap();
+
+        led_red_pin.set_high().unwrap();
         // Set RGB LED to white
-        ws.write([WHITE].iter().copied()).unwrap();
+        delay.delay_ms(500);
+        led_red_pin.set_low().unwrap();
+        // Set RGB LED to white
         delay.delay_ms(500);
     }
 }
